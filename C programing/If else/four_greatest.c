3bc2e701d9aa9432cd9bb9e33b72a02b922af35a
AryanHub9 <ARYANKUMAR4972@GMAIL.COM>
2024-03-23 23:27:24 +0530

    update

---
+++ C programing/If else/four_greatest.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    int b;
+    int c;
+    int d;
+    printf("enter 1st no:");
+    scanf("%d",&a);
+    printf("enter 2nd no:");
+    scanf("%d",&b);
+    printf("enter 3rd no:");
+    scanf("%d",&c);
+    printf("enter 4th no:");
+    scanf("%d",&d);
+    if(a>b&&a>c&&a>d)
+    {
+        printf("%d is greatest",a);
+    }
+    if(b>a&&b>c&&b>d)
+    {
+        printf("%d is greatest",b);
+    }
+    if(c>a&&c>b&&c>d)
+    {
+        printf("%d is greatest",c);
+    }
+    if(d>a&&d>b&&d>c)
+    {
+        printf("%d is greatest",d);
+    }
+    return 0;
+}
